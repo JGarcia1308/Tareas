4acc0ecf78cca285e67118e3f1215e8067443b4a
José García <jed130885@hotmail.com>
2022-02-05 17:46:01 -0600

    primer commit

---
+++ variables_estructuras.js
@@ -0,0 +1,14 @@
+var mes = 10;
+
+if (mes == 1 || mes == 2 || mes == 12) {
+    console.log("Es Invierno");
+} else if (mes == 3 || mes == 4 || mes == 5) {
+    console.log("Es Primavera");
+} else if (mes == 6 || mes == 7 || mes == 8) {
+    console.log("Es Verano");
+} else if (mes == 9 || mes == 10 || mes == 11) {
+    console.log("Es Otoño");
+}
+
+console.log("Carné: 07243032");
+console.log("Nombre: José Eduardo García Chávez");
